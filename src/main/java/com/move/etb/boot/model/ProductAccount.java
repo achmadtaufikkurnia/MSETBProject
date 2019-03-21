fa70ebb4b158cdab529a8276a7cc5ab4b63fa187
achmadtaufikkurnia <achmadtaufikkurnia@gmail.com>
2019-03-21 18:57:14 +0700

    add eureka

--- src/main/java/com/move/etb/boot/model/ProductAccount.java
+++ src/main/java/com/move/etb/boot/model/ProductAccount.java
@@ -2,74 +2,69 @@ package com.move.etb.boot.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "DBO.IB_PRODUCT_ACCOUNT")
+//@Entity
+//@Table(name = "DBO.IB_PRODUCT_ACCOUNT")
 public class ProductAccount implements Serializable {
 
 	private static final long serialVersionUID = -3093494878413148877L;
 	
-	@Id
-	@Column(name = "OID")
-	private Integer oid;
-	
-	@Column(name = "PRODUCT_CODE")
-	private String productCode;
-	@Column(name = "PRODUCT_NAME")
-	private String productName;
-	@Column(name = "DEBITED")
-	private Integer debited;
-	@Column(name = "CREDITED")
-	private Integer credited;
-	@Column(name = "STATUS")
-	private Integer status;
-	@Column(name = "MK_PENDING")
-	private Integer mkPending;
-	
-	public Integer getOid() {
-		return oid;
-	}
-	public void setOid(Integer oid) {
-		this.oid = oid;
-	}
-	public String getProductCode() {
-		return productCode;
-	}
-	public void setProductCode(String productCode) {
-		this.productCode = productCode;
-	}
-	public String getProductName() {
-		return productName;
-	}
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-	public Integer getDebited() {
-		return debited;
-	}
-	public void setDebited(Integer debited) {
-		this.debited = debited;
-	}
-	public Integer getCredited() {
-		return credited;
-	}
-	public void setCredited(Integer credited) {
-		this.credited = credited;
-	}
-	public Integer getStatus() {
-		return status;
-	}
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-	public Integer getMkPending() {
-		return mkPending;
-	}
-	public void setMkPending(Integer mkPending) {
-		this.mkPending = mkPending;
-	}
+//	@Id
+//	@Column(name = "OID")
+//	private Integer oid;
+//	
+//	@Column(name = "PRODUCT_CODE")
+//	private String productCode;
+//	@Column(name = "PRODUCT_NAME")
+//	private String productName;
+//	@Column(name = "DEBITED")
+//	private Integer debited;
+//	@Column(name = "CREDITED")
+//	private Integer credited;
+//	@Column(name = "STATUS")
+//	private Integer status;
+//	@Column(name = "MK_PENDING")
+//	private Integer mkPending;
+//	
+//	public Integer getOid() {
+//		return oid;
+//	}
+//	public void setOid(Integer oid) {
+//		this.oid = oid;
+//	}
+//	public String getProductCode() {
+//		return productCode;
+//	}
+//	public void setProductCode(String productCode) {
+//		this.productCode = productCode;
+//	}
+//	public String getProductName() {
+//		return productName;
+//	}
+//	public void setProductName(String productName) {
+//		this.productName = productName;
+//	}
+//	public Integer getDebited() {
+//		return debited;
+//	}
+//	public void setDebited(Integer debited) {
+//		this.debited = debited;
+//	}
+//	public Integer getCredited() {
+//		return credited;
+//	}
+//	public void setCredited(Integer credited) {
+//		this.credited = credited;
+//	}
+//	public Integer getStatus() {
+//		return status;
+//	}
+//	public void setStatus(Integer status) {
+//		this.status = status;
+//	}
+//	public Integer getMkPending() {
+//		return mkPending;
+//	}
+//	public void setMkPending(Integer mkPending) {
+//		this.mkPending = mkPending;
+//	}
 }
