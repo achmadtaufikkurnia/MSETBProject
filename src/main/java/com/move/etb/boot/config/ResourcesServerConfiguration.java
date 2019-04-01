08edb789d8ca9b11d1923e4d1f49f6354477b7ce
achmadtaufikkurnia <achmadtaufikkurnia@gmail.com>
2019-04-01 19:13:27 +0700

    add oauth2

---
+++ src/main/java/com/move/etb/boot/config/ResourcesServerConfiguration.java
@@ -0,0 +1,50 @@
+package com.move.etb.boot.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+@EnableResourceServer
+@Configuration
+public class ResourcesServerConfiguration extends ResourceServerConfigurerAdapter  {
+
+	
+	@Bean
+	@ConfigurationProperties(prefix = "api.datasource")
+	public DataSource apiDataSource() {
+		return DataSourceBuilder.create().build();
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		TokenStore tokenStore = new JdbcTokenStore(apiDataSource());
+		resources.resourceId("product_api").tokenStore(tokenStore);
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/actuator/**").permitAll()
+		.antMatchers("/health").permitAll()
+		.anyRequest().authenticated()
+		.and().headers().addHeaderWriter((request, response) -> {
+				response.addHeader("Access-Control-Allow-Origin", "*");
+				if (request.getMethod().equals("OPTIONS")) {
+					response.setHeader("Access-Control-Allow-Methods",
+							request.getHeader("Access-Control-Request-Method"));
+					response.setHeader("Access-Control-Allow-Headers",
+							request.getHeader("Access-Control-Request-Headers"));
+				}
+			});
+	}
+	
+}
---
+++ src/main/java/com/move/etb/boot/util/CommonUtils.java
@@ -0,0 +1,18 @@
+package com.move.etb.boot.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CommonUtils {
+
+	public final static String YYYYMMDDHHMMSSX = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+	
+	public static String getDateToString(Date date,String format) {
+		SimpleDateFormat dateFormat = null;
+		String result = null;
+		dateFormat = new SimpleDateFormat(format);
+		result = dateFormat.format(date);
+		return result;
+	}
+	
+}
