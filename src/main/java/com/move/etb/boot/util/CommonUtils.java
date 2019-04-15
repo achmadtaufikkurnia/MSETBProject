5174b65cbb9e115f290cf4e60c83a9bcadb78cc7
achmadtaufikkurnia <achmadtaufikkurnia@gmail.com>
2019-04-15 18:24:29 +0700

    add config

--- src/main/java/com/move/etb/boot/util/CommonUtils.java
+++ src/main/java/com/move/etb/boot/util/CommonUtils.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class CommonUtils {
 
-	public final static String YYYYMMDDHHMMSSX = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+	public final static String YYYYMMDDHHMMSSX = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ";
 	
 	public static String getDateToString(Date date,String format) {
 		SimpleDateFormat dateFormat = null;
